8fb9c0b5494fcfe37a2b38e197b9e940ddac4ea9
Dercia Silva <dercia.silva@gmail.com>
2023-02-09 15:47:50 +0000

    test: clicking add note button should open a new note

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,8 +5,9 @@ import {
 } from './test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import App from './App';
+import { logRoles } from '@testing-library/react';
 
-test('clicking a note in the list shows its content in the editor', async () => {
+test('clicking a note in the list should show its content in the editor', async () => {
   const user = userEvent.setup();
 
   render(<App />);
@@ -15,19 +16,64 @@ test('clicking a note in the list shows its content in the editor', async () => 
   const listItemText = listItems.map((li) => li.textContent);
   expect(listItemText).toEqual(initialNotes.map((n) => n.title));
 
-  await user.click(listItems[1]);
+  const noteIdx = 1;
+  await user.click(listItems[noteIdx]);
 
   const titleInput = screen.getByPlaceholderText(/note title/i);
-  expect(titleInput).toHaveValue(initialNotes[1].title);
+  expect(titleInput).toHaveValue(initialNotes[noteIdx].title);
 
   const contentEditor = screen.getByRole('textbox', { name: /content/i });
-  expect(contentEditor).toHaveTextContent(initialNotes[1].content);
+  expect(contentEditor).toHaveTextContent(initialNotes[noteIdx].content);
 
-  await user.click(listItems[0]);
-  expect(titleInput).toHaveValue(initialNotes[0].title);
-  expect(contentEditor).toHaveTextContent(initialNotes[0].content);
+  const note2Idx = 2;
+  await user.click(listItems[note2Idx]);
+  expect(titleInput).toHaveValue(initialNotes[note2Idx].title);
+  expect(contentEditor).toHaveTextContent(initialNotes[note2Idx].content);
 
-  await user.click(listItems[2]);
-  expect(titleInput).toHaveValue(initialNotes[2].title);
-  expect(contentEditor).toHaveTextContent(initialNotes[2].content);
+  const note3Idx = 0;
+  await user.click(listItems[note3Idx]);
+  expect(titleInput).toHaveValue(initialNotes[note3Idx].title);
+  expect(contentEditor).toHaveTextContent(initialNotes[note3Idx].content);
+});
+
+test('clicking add note button should open a new note', async () => {
+  const user = userEvent.setup();
+
+  render(<App />);
+
+  const listItems = screen.getAllByRole('listitem');
+  const noteIdx = 1;
+  await user.click(listItems[noteIdx]);
+
+  const titleInput = screen.getByPlaceholderText(/note title/i);
+  expect(titleInput).toHaveValue(initialNotes[noteIdx].title);
+
+  const contentEditor = screen.getByRole('textbox', { name: /content/i });
+  expect(contentEditor).toHaveTextContent(initialNotes[noteIdx].content);
+
+  // click add note button
+  const addButton = screen.getByRole('button', { name: 'Add Note' });
+  await user.click(addButton);
+
+  // note view is updated
+  expect(titleInput).toHaveValue('');
+  expect(contentEditor).toHaveTextContent('');
+
+  // type in new note content
+  const newTitle = 'Test list item :D';
+  // const newContent = 'New content.';
+  await user.type(titleInput, newTitle);
+  // await user.type(contentEditor, newContent);
+
+  // note view is updated
+  expect(titleInput).toHaveValue(newTitle);
+  // expect(contentEditor).toHaveTextContent(newContent);
+
+  // switch to another note on the list
+  const note2Idx = 0;
+  await user.click(listItems[note2Idx]);
+
+  // note view is updated
+  expect(titleInput).toHaveValue(initialNotes[note2Idx].title);
+  expect(contentEditor).toHaveTextContent(initialNotes[note2Idx].content);
 });
