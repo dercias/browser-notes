42d3b6ebc6238e15c7657c99f0353d6783e70a23
Dercia Silva <dercia.silva@gmail.com>
2023-02-22 16:21:44 +0000

    test: fixed test

--- src/routes/home/home.test.tsx
+++ src/routes/home/home.test.tsx
@@ -9,7 +9,8 @@ test('welcome note is shown on first run', async () => {
   renderWithBrowser(<Home />);
 
   const sidebar = screen.getByRole('complementary');
-  const listItems = within(sidebar).getByRole('listitem');
+  const listItems = within(sidebar).getAllByRole('listitem');
 
-  expect(listItems).toHaveTextContent(/hello world/i);
+  expect(listItems[0]).toHaveTextContent(/hello world/i);
+  expect(listItems[1]).toHaveTextContent(/A second note/i);
 });
