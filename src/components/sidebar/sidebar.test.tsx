31dda9e6cacb3528c358223bedc733882f88d8d3
Dercia Silva <dercia.silva@gmail.com>
2023-03-09 10:57:48 +0000

    test: show all notes when add button is clicked

--- src/components/sidebar/sidebar.test.tsx
+++ src/components/sidebar/sidebar.test.tsx
@@ -14,6 +14,14 @@ const initialNotes = [
     createdAt: 1675766577,
     updatedAt: 1675766578,
   },
+  {
+    id: '1',
+    title: 'Mauris sed sapien at diam sollicitudin',
+    content: 'Lorem ipsum',
+    starred: true,
+    createdAt: 1675766577,
+    updatedAt: 1675766578,
+  },
 ];
 
 const initialState = {
@@ -34,4 +42,37 @@ test('clicking add note button should create a new list item', async () => {
   const list = screen.getByTestId('notes-list');
   const listItems = within(list).getAllByRole('listitem');
   expect(listItems).toHaveLength(initialNotes.length + 1);
+});
+
+test('show all notes when add button is clicked', async () => {
+  const user = userEvent.setup();
+
+  renderWithBrowser(<Sidebar />, { preloadedState: initialState });
+
+  // Show starred notes and add a note
+  const starredListItem = screen.getByTestId('sidebar-nav-item-starred');
+  await user.click(starredListItem);
+
+  const list = screen.getByTestId('notes-list');
+  const listItems = within(list).getAllByRole('listitem');
+
+  const starredCount = initialNotes.filter((n) => n.starred).length;
+
+  expect(listItems).toHaveLength(starredCount);
+
+  const addButton = screen.getByTestId('add-note-button');
+  await user.click(addButton);
+
+  const list2 = screen.getByTestId('notes-list');
+  const listItems2 = within(list2).getAllByRole('listitem');
+  expect(listItems2).toHaveLength(initialNotes.length + 1);
+
+  // Show trashed notes and add a note
+  const trashListItem = screen.getByTestId('sidebar-nav-item-trash');
+  await user.click(trashListItem);
+  await user.click(addButton);
+
+  const list3 = screen.getByTestId('notes-list');
+  const listItems3 = within(list3).getAllByRole('listitem');
+  expect(listItems3).toHaveLength(initialNotes.length + 2);
 });
